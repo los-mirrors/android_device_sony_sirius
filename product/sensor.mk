4172fb8bfd44039530f70ad0def57989e27f9092
Max Weffers <rcstar6696@gmail.com>
2018-04-04 16:31:44 +0200

    sirius: Move sensor_def_qcomdev back to system
    * fix broken sensors
    
    Change-Id: I083a86ffa95f7a534c30346e71fdac618b01da6c

--- product/sensor.mk
+++ product/sensor.mk
@@ -1,6 +1,6 @@
 # Sensor
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/sensor/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensor_def_qcomdev.conf 
+   $(LOCAL_PATH)/sensor/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
