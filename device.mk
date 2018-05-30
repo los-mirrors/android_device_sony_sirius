0b40f9c60ff9931d6ba43a402517c9db87b39be6
Christian Oder <myself5@carbonrom.org>
2018-05-30 20:34:11 +0200

    sirius: Use shamu configs for dalvik vm and HWUI
    Change-Id: I3c4f33fdf51264677c51f0297996a796b9d36f16

--- device.mk
+++ device.mk
@@ -25,9 +25,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Dalvik/HWUI
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+# setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# setup HWUI configs
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Include Vendor files
 $(call inherit-product, vendor/sony/sirius/sirius-vendor.mk)
